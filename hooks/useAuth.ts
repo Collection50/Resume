a9a03204a867a0a8bd3167906b49e235bc39c1d1
Collection50 <fkaus65@naver.com>
2023-11-09 10:20:38 +0900

    fix: 빌드 오류 해결

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,11 +1,13 @@
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export function useAuth() {
   const accessToken = typeof window !== 'undefined' ? sessionStorage.getItem('role') : null;
-
   const router = useRouter();
 
-  if (!accessToken) {
-    router.push('/login');
-  }
+  useEffect(() => {
+    if (!accessToken) {
+      router.push('/login');
+    }
+  });
 }
